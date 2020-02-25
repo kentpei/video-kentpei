d9d66a3778b81a7766f2fd2057ba8d5429b5cab4
kentpei <48425664+kentpei@users.noreply.github.com>
2020-02-24 20:10:17 -0500

    Update threads.py

--- threads.py
+++ threads.py
@@ -3,7 +3,8 @@ import time
 import threading
 import txtadded
 import queue
-def worker():
+import os
+def worker(i):
     while True:
         item = q.get()
         if item is None:
@@ -12,29 +13,36 @@ def worker():
         print(item + "'s video is made!" ) #show working procedure
         q.task_done()
 if __name__ == '__main__':
-    num_of_threads = 10 #acclerate the process
+    num_of_threads = 5 #acclerate the process
     Name_List = ["StephenCurry30", "KyrieIrving", "drose", "DWade", "CP3", "KlayThompson", "KDTrey5", "Dame_Lillard",
                  "KingJames", "AntDavis23"]
-
     q = queue.Queue()
 
     threads = []
     start = time.time()
 
-    for i in range(1, num_of_threads + 1):
-        t = threading.Thread(target=worker)
-        t.start()
-        threads.append(t)
-
     for item in Name_List:
         q.put(item)
+    for i in range(1, num_of_threads + 1):
+        t = threading.Thread(target=worker,args=(i,))
+        threads.append(t)
+        t.start()
 
     q.join() #when all task finsh , it joins
+    
 
     for i in range(num_of_threads):
         q.put(None)         # to stop workers
     for t in threads:
         t.join()
+    f = open("Mylist.txt", 'w')
+
+    for i in Name_List:
+        f.write('file ' + "\'" + str(i) + '.mp4' + "\'")
+        f.write('\n')
+    f.close()
+    os.system("ffmpeg -f concat -safe 0 -i Mylist.txt -c copy output.mp4")
+
     print(threads)
     end = time.time()
     print(end-start) #see how long it takes to finish all assignments
