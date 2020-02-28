93ba0926b3f1b8d40e8f35f4d95ce71af381aedf
kentpei <48425664+kentpei@users.noreply.github.com>
2020-02-27 20:49:47 -0500

    Update tweetinfo.py

--- tweetinfo.py
+++ tweetinfo.py
@@ -1,25 +1,47 @@
 #!/usr/bin/env python
 # encoding: utf-8
-
-
+import re
+import json
 import tweepy  # https://github.com/tweepy/tweepy
+import os
+import configparser
+import shutil
+shutil.copy('keys','keys.py')
+from keys import *
 
 # Twitter API credentials
-consumer_key = ''
-consumer_secret = ''
-access_key = ''
-access_secret = ''
-
+def get_oldJson(name):
+  text = []
+  with open('jsonFolder/' + name + '.json') as json_data:
+      data_dict = json.load(json_data)
+      emoji_pattern = re.compile("["
+                                 u"\U0001F600-\U0001F64F"  # emoticons
+                                 u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                                 u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                                 u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                                 "]+", flags=re.UNICODE)
+  for line in data_dict:
+      line = emoji_pattern.sub(r'', line)
+      text.append(line)
+  return text
 
 def get_all_tweets(name,n):
-    # Twitter only allows access to a users most recent 3240 tweets with this method
-
-    # authorize twitter, initialize tweepy
+    # Here for testing keys exist or not
+    try:
+        f = open('keys') #wrong name or file not exist will return the json file
+        print('keys open')
+        f.close()
+    except IOError:
+        print("File error")
+        return get_oldJson(name)
+    # check for whether keys file is empty or not first
+    if (os.stat('keys').st_size <= 0):
+        print('No content in the key file')
+        # call function for saved json file
+        return get_oldJson(name)
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_key, access_secret)
     api = tweepy.API(auth)
-
-    # initialize a list to hold all the tweepy Tweets
     alltweets = []
 
     # make initial request for most recent tweets (200 is the maximum allowed count)
@@ -27,4 +49,3 @@ def get_all_tweets(name,n):
     for tweet in new_tweets:
         alltweets = alltweets + [tweet.text]
     return alltweets
-
